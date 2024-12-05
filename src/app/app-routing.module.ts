1ada66ee776ae844f57b5b4cdce6210240d1b205
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-12-04 23:15:01 -0300

    wip: permissao para acessar o privado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {hasSessionGuard} from "@app/guards/has-session.guard";
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
   },
   {
     path: 'painel',
-    loadChildren: () => import('./views/private/private.module').then(m => m.PrivateModule)
+    loadChildren: () => import('./views/private/private.module').then(m => m.PrivateModule),
+    canActivate: [hasSessionGuard]
   },
   {
     path: 'session',
