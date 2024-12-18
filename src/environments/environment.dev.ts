ebf5f8669585cd2737ed88921567b321dbff9890
emilsonsn <emilsonsn2@gmail.com>
2024-12-18 14:57:05 -0300

    setup: configura envioments do sistema

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,7 +5,7 @@ export const environment = {
   appName: 'Zodarium App',
   public: "/",
   private: "/painel",
-  api: 'https://zodiarium.com:3000/api',
+  api: 'https://zodiarium.com:3001/api',
   // api: 'http://127.0.0.1:8000/api',
   version: require('../../package.json').version
 };
